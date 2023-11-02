90efd4e38553b510096bc37e6145e315eea2af44
logeshwarand <47322586+thelogeshwaran@users.noreply.github.com>
2023-11-02 10:43:35 +0530

    fetching data from backend

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,49 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
 
-const Home: NextPage = () => {
+type Data = {
+  friends: Record<string, string>;
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  let friends;
+
+  try {
+    const response = await fetch(
+      process.env.NEXT_PUBLIC_HASURA_PROJECT_ENDPOINT as string,
+      {
+        method: "POST",
+        headers: {
+          "x-hasura-admin-secret": process.env.HASURA_ADMIN_SECRET as string,
+        },
+        body: JSON.stringify({
+          query: `query {
+          friend {
+            name
+          }
+        }`,
+        }),
+      }
+    );
+
+    const result = await response.json();
+
+    friends = result.data.friend;
+  } catch (e) {
+    console.log(e);
+  }
+
+  return {
+    props: { friends },
+  };
+};
+
+const Home: NextPage = ({
+  friends,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,44 +53,9 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {friends.map((item: any) => (
+          <div key={item.id}>{item?.name}</div>
+        ))}
       </main>
 
       <footer className={styles.footer}>
@@ -59,14 +64,14 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
